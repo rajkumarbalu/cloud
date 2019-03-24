62eaf2a27ed7331a12432addde4b20e16506bf7d
Deva <rajkumar.balu@yahoo.co.in>
2019-03-24 12:37:25 +0530

    Modified zuul filter

--- zuulservice/src/main/java/com/service/zuulservice/ZuulAuthFilter.java
+++ zuulservice/src/main/java/com/service/zuulservice/ZuulAuthFilter.java
@@ -8,24 +8,22 @@ public class ZuulAuthFilter extends ZuulFilter{
 	@Override
 	public Object run() throws ZuulException {
 		String auth = "Authenticating successful";
+		System.out.println("$$$$$$$$$$$$$$$$ZUUL FILTER INVOKED$$$$$$$$$$$$$$$$$$$");
 		return null;
 	}
 
 	@Override
 	public boolean shouldFilter() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	@Override
 	public int filterOrder() {
-		// TODO Auto-generated method stub
 		return 1;
 	}
 
 	@Override
 	public String filterType() {
-		// TODO Auto-generated method stub
 		return "pre";
 	}
 
